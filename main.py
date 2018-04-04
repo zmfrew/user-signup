59fd76368c096a27f8d7793a3e71e96f63c0109b
zfrew <z_frew@yahoo.com>
2018-04-04 15:51:10 -0500

    fix error functionality and add verification welcome page

--- main.py
+++ main.py
@@ -3,7 +3,25 @@ import cgi
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['DEBUG'] = True      
+
+def error_check(text):
+    if " " in text or 2 > len(text) or len(text) > 20:
+        return True
+    else:
+        return False
+
+def password_check(pass1, pass2):
+    if not pass1 == pass2 or pass2 == "":
+        return True
+    else:
+        return False
+
+def verify_email(email):
+    if not "@" in email or not "." in email or 2 > len(email) > 20:
+        return True
+    else:
+        return False
 
 @app.route("/signup", methods=['POST'])
 def signup():
@@ -17,21 +35,24 @@ def signup():
     verify_password_error = ""
     email_error = ""
 
-    if " " in username or 2 > len(username) or len(username) > 20:
+    if error_check(username):
         username_error = "That's not a valid username"
 
-    if " " in password or 2 > len(password) or len(password) > 20:
+    if error_check(password):
         password_error = "That's not a valid password"
 
-    if not password == verify_password or verify_password == "":
+    if password_check(password, verify_password):
         verify_password_error = "Passwords don't match"
 
-# TODO: fix this function so that it returns no error for the true emails    
-    if not email == "":
-        if not "@" in email or "." in email or 2 < len(email) or len(email) < 20:
-            email_error = "That's not a valid email"
+    if not email == "" and verify_email(email):
+        email_error = "That's not a valid email"
 
-    return render_template('base.html', username_error=username_error, password_error=password_error, verify_password_error=verify_password_error, email_error=email_error, username=username, password=password, verify_password=verify_password, email=email)
+    if error_check(username) == False and error_check(password) == False and password_check(password, verify_password) == False:
+       return render_template('welcome.html', username=username)
+
+    return render_template('base.html', username_error=username_error, password_error=password_error, 
+        verify_password_error=verify_password_error, email_error=email_error, username=username, 
+        password=password, verify_password=verify_password, email=email)
 
 @app.route("/")
 def index():
